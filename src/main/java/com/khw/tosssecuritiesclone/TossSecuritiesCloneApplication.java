4079ff9610fdcde5f54ee33e254a1db8e6b77a6f
oasis791 <khwi791@naver.com>
2025-10-06 13:35:34 +0900

    Initial Commit

---
+++ src/main/java/com/khw/tosssecuritiesclone/TossSecuritiesCloneApplication.java
@@ -0,0 +1,13 @@
+package com.khw.tosssecuritiesclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TossSecuritiesCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TossSecuritiesCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khw/tosssecuritiesclone/TossSecuritiesCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khw.tosssecuritiesclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TossSecuritiesCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
